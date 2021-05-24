5f6236a8bb94afd10305ca0fa90218b19ac2436c
RIKU <83934713+r-s-kyu@users.noreply.github.com>
2021-05-25 00:23:50 +0900

    いくつかコンポーネント分割を行いTODOrリストが完成

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -4,6 +4,7 @@ export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
     <div className="input-area">
+      {/* {console.log("コンポーネント回す")} */}
       <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
       <button onClick={onClick}>追加</button>
     </div>
